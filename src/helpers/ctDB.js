b68659dcaf470f16a3a2f21d79e5c76193000f8d
Charles Morgan <charlesmorgan@c13.local>
2018-04-09 22:38:21 -0700

    fix vscode/eslint/prettier config issues

--- src/helpers/ctDB.js
+++ src/helpers/ctDB.js
@@ -1,5 +1,28 @@
-import axios from "axios"
 
-async function queryDB({destructureHere}) {
-  
-}
+let isVirginBool, categoryString
+
+export default queryStringLegendObj = (function() {
+  return {
+    searchByCocktailName: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita',
+    searchByIngredientName: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka',
+    searchByIngredient: 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin',
+    getRandomCocktail: 'https://www.thecocktaildb.com/api/json/v1/1/random.php',
+    getFullCocktailDetailsById: 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=13060',
+    filterByAlcoholPresence: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${
+      isVirginBool ? 'Non_Alcoholic' : 'Alcoholic'
+    }`,
+    filterByCategory: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoryString}`,
+    filterByGlassType: 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=Cocktail_glass',
+    listAllPossibleFiltersByType: {
+      categories: 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list',
+      glasses: 'https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list',
+      ingredients: 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list',
+      alcoholic: 'https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list'
+    },
+    getIngredientImagesbySize: {
+      small: 'https://www.thecocktaildb.com/images/ingredients/ice-Small.png',
+      medium: 'https://www.thecocktaildb.com/images/ingredients/ice-Medium.png',
+      large: 'https://www.thecocktaildb.com/images/ingredients/ice.png'
+    }
+  }
+})()
