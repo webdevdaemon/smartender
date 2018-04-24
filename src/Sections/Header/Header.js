57aabdb6d654fdbb2955cfdcb68fd4bf779b618c
Charles Morgan <charlesmorgan@c13.local>
2018-04-24 15:12:49 -0700

    coding pre-session sync w/remote

--- src/Sections/Header/Header.js
+++ src/Sections/Header/Header.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const BackButton = () => (
+  <div className="back-button">
+    <button className="back-btn">
+      <span>{'&lt;'}</span>
+    </button>
+  </div>
+)
+
 const Header = ({ children }) => (
   <div className="header">
+    <BackButton />
     {children}
   </div>
 )
