c426b8cccf9331b08134c9225feb9c57c1f84916
Charles Morgan <charlesmorgan@c13.local>
2018-05-01 18:55:06 -0700

    session init--TASK: search/autocomplete helper

--- src/_helpers/searchAutocomplete.js
+++ src/_helpers/searchAutocomplete.js
@@ -1,24 +1,46 @@
 import axios from 'axios'
 import { get } from './dbQueries'
+import { prependOnceListener } from 'cluster';
 
-const wordSearch
+const initMatches = []
 
-const firstLetter = function (searchQuery = '') {
-  let _l = searchQuery.length
-  const matches = []
+/*  ********************************************* /
+[POSSIBLE FEATURE] "Variable Search Precision"
+- search levels 1-3 from "fuzzy" to precise
+    *********************************************
+const getRegExByLevel = (stringToMatch) => (lvl = 3) => [
+  new RegX(`[${stringToMatch}]`, 'gi'),
+  // FUZZY - '/[abc]/'
+  new RegX(`${stringToMatch}`,   'gi' ),
+  // LOOSE   '/abc/'
+  new RegX(`^${stringToMatch}`,  'gi' ),
+  // PRECISE '/^abc/' 
+][lvl-1]
+**  ********************************************  */
 
-  if (_l === 1) {
-    axios(searchQuery)
-      .then(({ data: { drinks } }) => drinks)
-      .then(drinks =>
-        drinks
-          .map(({ strDrink }) => strDrink)
-          .filter() // ?
-          .forEach(match => matches.push(match)))
-  } else {
-    homeIn()
-  }
-  console.log(matches)
+const filterResults = stringToMatch => {
+  const rgxToMatch = new RegExp(`(${stringToMatch})`, 'gi')
+  return listToFilter =>
+    listToFilter.filter(str => rgxToMatch.test(str))
 }
 
-firstLetter(get.drinkByName('v'))
+const initAutocompList = qry =>
+  axios(get.drinkByName(qry))
+    .then(({ data: { drinks } }) => drinks)
+    .then(drinks => drinks.map(({ strDrink }) => strDrink))
+    .then(list => {
+      list.forEach(match => initMatches.push(match))
+      console.log({ list, initMatches: [...initMatches] })
+      return list
+    })
+
+const go = (searchQ, current) =>
+  searchQ.length > 0
+    ? searchQ.length !== 1
+      ? filterResults(searchQ, current)
+      : initAutocompList(searchQ)
+    : null // no query = return <null>
+
+let ___ = go('v')
+___ = go('vo', ___)
+
