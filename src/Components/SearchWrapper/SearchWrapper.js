5c5476820e541e1175e6bb401c1f57087d887f83
Charles Morgan <charlesmorgan@c13.local>
2018-10-04 23:21:07 -0700

    fix naming error in cacheUtils.js

--- src/Components/SearchWrapper/SearchWrapper.js
+++ src/Components/SearchWrapper/SearchWrapper.js
@@ -1,7 +1,7 @@
 /**
-* @augments {Component<{		match:object,	>}
-*/
-import React, { Component } from 'react'
+ * @augments {Component<{		match:object,	>}
+ */
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 import autoComp from '../../_helpers/searchModule'
@@ -10,48 +10,47 @@ import SearchResultsListWrapper from '../SearchResultsListWrapper'
 import SearchFormWrapper from '../SearchFormWrapper'
 
 class SearchWrapper extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			searchString: '',
-			listResults: [],
-			cache: {},
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchString: '',
+      listResults: [],
+      cache: {},
+    }
+  }
 
-	static propTypes = {
-		match: PropTypes.object,
+  static propTypes = {
+    match: PropTypes.object,
+  }
 
-	}
+  runAutoComp = str =>
+    autoComp(str)
+      .then(list => {
+        this.setState({listResults: list})
+        this.setState({cache: {...this.state.cache, [str]: list}})
+      })
+      .catch(err => new Error(err.message))
 
-	runAutoComp = (str) =>
-		autoComp(str)
-			.then((list) => {
-				this.setState({ listResults: list })
-				this.setState({ cache: { ...this.state.cache, [str]: list } })
-			})
-			.catch((err) => new Error(err.message))
+  updateUI = evt => {
+    const searchString = evt.target.value
+    this.setState({searchString})
+    if (this.state.cache.hasOwnProperty(searchString)) {
+      this.setState({listResults: this.state.cache[searchString]})
+    } else {
+      this.runAutoComp(searchString)
+    }
+  }
 
-	updateUI = (evt) => {
-		const searchString = evt.target.value
-		this.setState({ searchString })
-		if (this.state.cache.hasOwnProperty(searchString)) {
-			this.setState({ listResults: this.state.cache[searchString] })
-		} else {
-			this.runAutoComp(searchString)
-		}
-	}
-
-	render() {
-		const { searchString, listResults } = this.state
-		return (
-			<div className="search-wrapper">
-				<SearchFormWrapper searchString={searchString} updateUI={this.updateUI} />
-				<hr />
-				<SearchResultsListWrapper listResults={listResults} match={this.props.match} />
-			</div>
-		)
-	}
+  render() {
+    const {searchString, listResults} = this.state
+    return (
+      <div className="search-wrapper">
+        <SearchFormWrapper searchString={searchString} updateUI={this.updateUI} />
+        <hr />
+        <SearchResultsListWrapper listResults={listResults} match={this.props.match} />
+      </div>
+    )
+  }
 }
 
 export default SearchWrapper
--- src/_helpers/cacheUtils.js
+++ src/_helpers/cacheUtils.js
@@ -25,7 +25,7 @@ const searchUtils = (() => {
   }
 })()
 
-export default cacheUtils
+export default searchUtils
 
 
 
